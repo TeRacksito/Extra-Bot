727228512be1860d51424e577d3b7b46150faf79
Ahmed <ahmedthecat3457@gmail.com>
2022-07-27 20:09:17 +0300

    Slightly improved the help command

--- cogs/help.py
+++ cogs/help.py
@@ -9,7 +9,7 @@ class help(commands.Cog):
     #Help command
     @commands.command()
     async def help(self, ctx):
-        embed = nextcord.Embed(title="Commands" ,color=0x2852fa,description="-help - Display This Message \n-kick [Member] [Reason] - Kick A Spesific Member \n-spam [Message] [Number] - Spam A Spesific Message A Number Of Times \n-ytSearch [Query] - Search On Youtube For Query \n-gSearch [Quers] - Search On Google For Query \n-Myid - Tell You Your Id \n-Lyrics [Artist] [Song] - Search For A Song's Lyrics By Artist")
+        embed = nextcord.Embed(title="Commands" ,color=0x2852fa,description=f"-help - Display This Message \n-kick [Member] [Reason] - Kick A Spesific Member \n-spam [Message] [Number] - Spam A Spesific Message A Number Of Times \n-ytSearch [Query] - Search On Youtube For Query \n-gSearch [Quers] - Search On Google For Query \n-Myid - Tell You Your Id \n-Lyrics [Artist] [Song] - Search For A Song's Lyrics By Artist \n Requsted By <@{ctx.author.id}>")
         await ctx.send(embed=embed)
 
 #setup
