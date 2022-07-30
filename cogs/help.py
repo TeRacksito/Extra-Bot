697b5744d2aed80839e7b9f79f781180d3e92911
Ahmed <ahmedthecat3457@gmail.com>
2022-07-30 21:12:07 +0300

    Fixed A Misspelling

--- cogs/help.py
+++ cogs/help.py
@@ -12,8 +12,9 @@ class help(commands.Cog):
         embed = nextcord.Embed(
         title="Commands",
         color=0x2852fa,
-        description=f"-help - Display This Message \n-kick [Member] [Reason] - Kick A Spesific Member \n-spam [Message] [Number] - Spam A Spesific Message A Number Of Times \n-ytSearch [Query] - Search On Youtube For Query \n-gSearch [Quers] - Search On Google For Query \n-Myid - Tell You Your Id \n-Lyrics [Artist] [Song] - Search For A Song's Lyrics By Artist \n-YesOrNo [Question] - Ask The Bot A Question \n-CheckMcName [Name] - Check If A Minecraft Name Is Avaible Or Not  \n \n Requested By <@{ctx.author.id}>")
+        description=f"-help - Display This Message \n-kick [Member] [Reason] - Kick A Spesific Member \n-spam [Message] [Number] - Spam A Spesific Message A Number Of Times \n-ytSearch [Query] - Search On Youtube For Query \n-gSearch [Query] - Search On Google For Query \n-Myid - Tell You Your Id \n-Lyrics [Artist] [Song] - Search For A Song's Lyrics By Artist \n-YesOrNo [Question] - Ask The Bot A Question \n-CheckMcName [Name] - Check If A Minecraft Name Is Availbe Or Not  \n \n Requested By <@{ctx.author.id}>")
         await ctx.send(embed=embed)
+        print("use")
 
 #setup
 def setup(client):
