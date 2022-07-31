8cd7ef447ba891f3cd6c54770290fb56679673bd
Ahmed <ahmedthecat3457@gmail.com>
2022-07-31 14:09:09 +0300

    An Improvment

--- cogs/help.py
+++ cogs/help.py
@@ -13,7 +13,6 @@ class help(commands.Cog):
         color=0x2852fa,
         description=f"-help - Display This Message \n-kick [Member] [Reason] - Kick A Spesific Member \n-spam [Message] [Number] - Spam A Spesific Message A Number Of Times \n-ytSearch [Query] - Search On Youtube For Query \n-gSearch [Query] - Search On Google For Query \n-Myid - Tell You Your Id \n-Lyrics [Artist] [Song] - Search For A Song's Lyrics By Artist \n-YesOrNo [Question] - Ask The Bot A Question \n-CheckMcName [Name] - Check If A Minecraft Name Is Availbe Or Not  \n \n Requested By <@{ctx.author.id}>")
         await ctx.send(embed=embed)
-        print("use")
 
 #setup
 def setup(client):
--- cogs/yesorno.py
+++ cogs/yesorno.py
@@ -8,7 +8,7 @@ class yesorno(commands.Cog):
 
     @commands.command(pass_context=True)
     async def yesorno(self, ctx , arg1):
-        answers = ["Yes", "No", "Of Course", "Of Course No", "I Can't Decide", "Si"]
+        answers = ["Yes", "No", "Of Course", "Of Course No", "I Can't Decide"]
         shuffle(answers)
         Embed = nextcord.Embed(title=arg1, description=f"{choice(answers)} \n \n Requested By <@{ctx.author.id}>", color=0x2852fa)
         await ctx.send(embed=Embed)
