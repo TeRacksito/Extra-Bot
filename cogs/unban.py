4a1c054534a7d10348fc173bcceecd51979f9ef8
Ahmed <ahmedthecat3457@gmail.com>
2022-08-06 17:21:19 +0300

    Bruh

--- cogs/unban.py
+++ cogs/unban.py
@@ -2,30 +2,30 @@ import nextcord
 from nextcord.ext import commands
 from nextcord.ext.commands import has_permissions
 
-class ban(commands.Cog):
+class unban(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    #ban command
+    #unban command
     @commands.command()
     @has_permissions(ban_members=True)
     async def unban(self,ctx,member: nextcord.Member):
         await member.unban()
-        await ctx.reply(f"UnBanned {member}")
+        await ctx.reply(f"Unbanned {member}")
         await member.send(f"You Got UNBan From The Server")
 
     #Exeption in case if the member doen't have the permissions
     @unban.error
     async def unban_error(self,ctx,error):
         if isinstance(error,commands.MissingPermissions):
-            await ctx.reply("You Don't Have The Permissions To Ban !")
+            await ctx.reply("You Don't Have The Permissions To unban !")
 
-    #ban Command Exception
-    @ban.error
-    async def ban_error(self,ctx , error):
+    #unban Command Exception
+    @unban.error
+    async def unban_error(self,ctx , error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(f"<@{ctx.author.id}> \nThis Command Usage Is ` -unban [member] `")
 
 #Setup 
 def setup(client):
-    client.add_cog(ban(client))
+    client.add_cog(unban(client))
