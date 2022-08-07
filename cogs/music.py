4c6183f38ad1516b8aede705a12dd6678a9021b2
Ahmed <ahmedthecat3457@gmail.com>
2022-08-07 21:42:06 +0300

    Added The Music Command

---
+++ cogs/music.py
@@ -0,0 +1,38 @@
+import nextcord 
+from nextcord.ext import commands
+import asyncio
+import youtube_dl
+
+class music(commands.Cog):
+    @commands.command(pass_context = True)
+    async def play(self, ctx, url):
+        yt_dl_options = {"format":"bestaudio/best"}
+        ffmpeg_options = {"options":"-vn"}
+
+        ytdl=youtube_dl.YoutubeDL(yt_dl_options)
+
+        if ctx.author.voice:
+            vc_client1 = await ctx.author.voice.channel.connect()
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download = False))
+
+            song = data['url']
+            player = nextcord.FFmpegPCMAudio(song, **ffmpeg_options)
+            vc_client1.play(player)
+            await ctx.reply(f"Now Playing The Requested Song By <@{ctx.author.id}>")
+        else:
+            await ctx.reply("You Are Not In A Voice Channel To Use This Command")
+
+    @commands.command(pass_context = True)
+    async def stop(self, ctx):
+        if ctx.author.voice:
+            await ctx.guild.voice_client.disconnect()
+        else:
+            await ctx.reply("The Bot/You Is Not In A Voice Channel To Stop !")
+
+
+
+#setup
+def setup(client):
+    client.add_cog(music(client))
+    
