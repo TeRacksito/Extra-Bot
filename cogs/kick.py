c78f17c10d3a2063689528a0b8de1fe35674f73b
Ahmed <ahmedthecat3457@gmail.com>
2022-08-20 09:20:38 +0300

    converted the kick command to a slash one

--- cogs/kick.py
+++ cogs/kick.py
@@ -1,33 +1,32 @@
 import nextcord
 from nextcord.ext import commands
 from nextcord.ext.commands import has_permissions
+from configparser import ConfigParser
 
-
+config=ConfigParser()
+config.read(".\config.ini")
+guild_id_1=config["options"]["guild1_id"]
+guild_id_2=config["options"]["guild2_id"]
+guilds=[int(guild_id_1),int(guild_id_2)]
 class Kick(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     # Kick command
-    @commands.command()
+    @nextcord.slash_command(guild_ids=guilds, description="Kick The Specified Member")
     @has_permissions(kick_members=True)
-    async def kick(self, ctx, member: nextcord.Member, *, reason=None):
-        await member.kick(reason=reason)
-        await ctx.reply(f"Kicked {member}\nReason:{reason}")
+    async def kick(self, interaction: nextcord.Interaction, member: nextcord.Member, *, reason: str = nextcord.SlashOption(description="The Reason You Want To Kick The Member", required=True)):
         await member.send(f"You Got Kick From The Server \nReason:{reason}")
+        await member.kick(reason=reason)
+        await interaction.response.defer()
+        await interaction.followup.send(f"Kicked {member}\nReason:{reason}")
 
     # Exeption in case if the member doen't have the permissions
     @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.reply("You Don't Have The Permissions To Kick !")
-
-    # Kick Command Exception
-    @kick.error
-    async def kick_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"<@{ctx.author.id}> \nThis Command Usage Is ` -kick [member] [reason] `")
-
-
+            
 # Setup
 def setup(client):
     client.add_cog(Kick(client))
