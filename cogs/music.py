ce1bb4cefcf20ed160bea25ae51a036bfd1b14cc
Ahmed <ahmedthecat3457@gmail.com>
2022-08-21 23:22:34 +0300

    Changed Music Commands To Slash

--- cogs/music.py
+++ cogs/music.py
@@ -1,35 +1,51 @@
 import asyncio
+from code import interact
 import nextcord
 import youtube_dl
 from nextcord.ext import commands
+from configparser import ConfigParser
+import time
 
+config=ConfigParser()
+config.read(".\config.ini")
+guild_id_1=config["options"]["guild1_id"]
+guild_id_2=config["options"]["guild2_id"]
+guilds=[int(guild_id_1),int(guild_id_2)]
 
 class Music(commands.Cog):
-    @commands.command(pass_context=True)
-    async def play(self, ctx, url):
+    @nextcord.slash_command(guild_ids=guilds, description="Play The Audio From The Specified Youtube Video")
+    async def play(self, interaction: nextcord.Interaction, url: str = nextcord.SlashOption(description="The Youtube Video Url You Want To Play Audio From", required=True)):
         yt_dl_options = {"format": "bestaudio/best"}
         ffmpeg_options = {"options": "-vn"}
 
         ytdl = youtube_dl.YoutubeDL(yt_dl_options)
 
-        if ctx.author.voice:
-            vc_client1 = await ctx.author.voice.channel.connect()
+        if interaction.user.voice:
+            vc_client1 = await interaction.user.voice.channel.connect()
             loop = asyncio.get_event_loop()
             data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
 
-            song = data['url']
+            song = data['url']  
             player = nextcord.FFmpegPCMAudio(song, **ffmpeg_options)
             vc_client1.play(player)
-            await ctx.reply(f"Now Playing The Requested Song By <@{ctx.author.id}>")
+            print("waiting 1 second")
+            time.sleep(1)
+            print("done")
+            await interaction.response.defer()
+            await interaction.followup.send("Play The Requested Song")
         else:
-            await ctx.reply("You Are Not In A Voice Channel To Use This Command")
+            await interaction.response.defer()
+            await interaction.followup.send("You Are Not In A Voice Channel To Use This Command")
 
-    @commands.command(pass_context=True)
-    async def stop(self, ctx):
-        if ctx.author.voice:
-            await ctx.guild.voice_client.disconnect()
+    @nextcord.slash_command(guild_ids=guilds, description="Stop The Audio Playing In The Current Channel")
+    async def stop(self, interaction: nextcord.Interaction):
+        if interaction.user.voice:
+            await interaction.guild.voice_client.disconnect()
+            await interaction.response.defer()
+            await interaction.followup.send(f"Stopped Audio Playing In The Current Voice Channel")
         else:
-            await ctx.reply("The Bot/You Is Not In A Voice Channel To Stop !")
+            await interaction.response.defer()
+            await interaction.followup.send("The Bot/You Is Not In A Voice Channel To Stop !")
 
 
 # setup
