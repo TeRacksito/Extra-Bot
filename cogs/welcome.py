d8e055ab1287ac3114d737ba3e202eb867695c48
Ahmed <ahmedthecat3457@gmail.com>
2022-09-04 22:17:28 +0300

    Added The Welcomer

---
+++ cogs/welcome.py
@@ -0,0 +1,25 @@
+import nextcord, sys
+from nextcord.ext import commands
+sys.path.insert(1, 'cogs\lib')
+import values as v
+
+guilds=v.values.getData("guilds")
+embedColor=v.values.getData("color")
+channel_var=v.values.getData("wlc_chnl")
+welcome_message=v.values.getData("wlc_message")
+server_name=v.values.getData("guild_name")
+
+class Welcome(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    #Command
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.client.get_channel(channel_var)
+        embed = nextcord.Embed(title=f"Hello Welcome To {server_name}",description=welcome_message)
+        await member.send(embed = embed)
+        await channel.send(f"Welcome To The Server, <@{member.id}>")
+# Setup
+def setup(client):
+    client.add_cog(Welcome(client))
