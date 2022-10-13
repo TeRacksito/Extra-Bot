a0843a80656409fbd94951609f77e41ba1cab79b
Ahmed <ahmedthecat3457@gmail.com>
2022-10-13 19:10:12 +0300

    Added flip command

---
+++ cogs/flip.py
@@ -0,0 +1,24 @@
+import nextcord, sys
+from nextcord.ext import commands
+import random
+sys.path.insert(1, 'cogs\lib')
+import values as v
+
+guilds=v.values.getData("guilds")
+embedColor=v.values.getData("color")
+
+class flipcoin(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @nextcord.slash_command(guild_ids=guilds, description="Flips a coin")
+    async def flipcoin(self, interaction: nextcord.Interaction):
+            choices = ["heads","tails"]
+            random.shuffle(choices)
+            final_choice = random.choice(choices)
+
+            await interaction.response.defer()
+            await interaction.followup.send(f"<@{interaction.user.id}> {final_choice}")
+# Setup
+def setup(client):
+    client.add_cog(flipcoin(client))
