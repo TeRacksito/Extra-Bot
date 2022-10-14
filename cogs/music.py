2ced8e070450508684747e4ad5c17f00b51bb7c5
Ahmed <ahmedthecat3457@gmail.com>
2022-10-14 15:33:45 +0300

    Fixed problems with /play command

--- cogs/music.py
+++ cogs/music.py
@@ -9,7 +9,7 @@ from nextcord.ext import commands
 
 guilds = v.values.getData("guilds")
 class Music(commands.Cog):
-    @nextcord.slash_command(guild_ids=guilds)
+    @nextcord.slash_command(guild_ids=guilds, description="Looks for the specified query on youtube and plays the most relevant result audio")
     async def play(self, interaction: nextcord.Interaction, qeury: str = nextcord.SlashOption(required=True)):
         yt_dl_options = {"format": "bestaudio/best"}
         ffmpeg_options = {"options": "-vn"}
@@ -17,6 +17,7 @@ class Music(commands.Cog):
         ytdl = youtube_dl.YoutubeDL(yt_dl_options)
 
         if interaction.user.voice:
+            await interaction.response.defer()
             url = YoutubeLib.GetVideoUrl(Qeury=qeury)
 
             vc_client1 = await interaction.user.voice.channel.connect()
@@ -26,19 +27,20 @@ class Music(commands.Cog):
             song = data['url']
             player = nextcord.FFmpegPCMAudio(song, **ffmpeg_options)
             vc_client1.play(player)
-            await interaction.response.defer()
             await interaction.followup.send(f"Now Playing The Requested Song By <@{interaction.user.id}>")
 
         else:
             await interaction.response.defer()
             await interaction.followup.send("You Are Not In A Voice Channel To Use This Command")
 
-    @commands.command(pass_context=True)
-    async def stop(self, ctx):
-        if ctx.author.voice:
-            await ctx.guild.voice_client.disconnect()
+    @nextcord.slash_command(guild_ids=guilds, description="Stops the audio playing in the current channel")
+    async def stop(self, interaction: nextcord.Interaction):
+        if interaction.user.voice:
+            await interaction.response.defer()
+            await interaction.guild.voice_client.disconnect()
+            await interaction.followup.send("Disconnected from the voice channel sucessfully.")
         else:
-            await ctx.reply("The Bot/You Is Not In A Voice Channel To Stop !")
+            await interaction.response.send_message("The Bot/You Is Not In A Voice Channel To Stop !")
 
 
 # setup
