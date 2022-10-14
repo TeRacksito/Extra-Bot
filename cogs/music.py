a6148c23fe2712a0f6b4ed641e80f3a57a8505d9
Ahmed <ahmedthecat3457@gmail.com>
2022-10-14 14:20:20 +0300

    Converted the play command to a slash command

--- cogs/music.py
+++ cogs/music.py
@@ -4,30 +4,34 @@ import youtube_dl
 import sys
 sys.path.insert(1, 'cogs\lib')
 from ytlib import YoutubeLib
+import values as v 
 from nextcord.ext import commands
 
+guilds = v.values.getData("guilds")
 class Music(commands.Cog):
-    @commands.command(pass_context=True)
-    async def play(self, ctx, qeury):
+    @nextcord.slash_command(guild_ids=guilds)
+    async def play(self, interaction: nextcord.Interaction, qeury: str = nextcord.SlashOption(required=True)):
         yt_dl_options = {"format": "bestaudio/best"}
         ffmpeg_options = {"options": "-vn"}
 
         ytdl = youtube_dl.YoutubeDL(yt_dl_options)
 
-        if ctx.author.voice:
+        if interaction.user.voice:
             url = YoutubeLib.GetVideoUrl(Qeury=qeury)
 
-            vc_client1 = await ctx.author.voice.channel.connect()
+            vc_client1 = await interaction.user.voice.channel.connect()
             loop = asyncio.get_event_loop()
             data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
 
             song = data['url']
             player = nextcord.FFmpegPCMAudio(song, **ffmpeg_options)
             vc_client1.play(player)
-            await ctx.reply(f"Now Playing The Requested Song By <@{ctx.author.id}>")
+            await interaction.response.defer()
+            await interaction.followup.send(f"Now Playing The Requested Song By <@{interaction.user.id}>")
 
         else:
-            await ctx.reply("You Are Not In A Voice Channel To Use This Command")
+            await interaction.response.defer()
+            await interaction.followup.send("You Are Not In A Voice Channel To Use This Command")
 
     @commands.command(pass_context=True)
     async def stop(self, ctx):
