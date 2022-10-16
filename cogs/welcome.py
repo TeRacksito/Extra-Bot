5243a8e7c6ec15b55ebe8b9c49d3dea11a01b766
Lukas Esc <55601315+Luk-ESC@users.noreply.github.com>
2022-10-16 22:55:54 +0200

    fixed some style issues

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,31 +1,34 @@
 import nextcord, sys
 from nextcord.ext import commands
-sys.path.insert(1, 'cogs\lib')
-import values as v
+from values import values
 
-guilds=v.values.getData("guilds")
-embedColor=v.values.getData("color")
-channel_var=v.values.getData("wlc_chnl")
-welcome_message=v.values.getData("wlc_message")
-server_name=v.values.getData("guild_name")
-welcomeServer=v.values.getData("wlc_enabled_guild")
+sys.path.insert(1, "cogs\lib")
+
+
+guilds = values.getData("guilds")
+embedColor = values.getData("color")
+channel_var = values.getData("wlc_chnl")
+welcome_message = values.getData("wlc_message")
+server_name = values.getData("guild_name")
+welcomeServer = values.getData("wlc_enabled_guild")
+
 
 class Welcome(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    #Command
+    # Command
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild.id
 
         if guild == welcomeServer:
             channel = self.client.get_channel(channel_var)
-            embed = nextcord.Embed(title=f"Hello Welcome To {server_name}",description=welcome_message)
-            await member.send(embed = embed)
+            embed = nextcord.Embed(title=f"Hello Welcome To {server_name}", description=welcome_message)
+            await member.send(embed=embed)
             await channel.send(f"Welcome To The Server, <@{member.id}>")
-        else:
-            pass
+
+
 # Setup
 def setup(client):
     client.add_cog(Welcome(client))
