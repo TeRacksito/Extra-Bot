c54a639ad8d859378b4fc7d602992620014db956
Lukas Esc <55601315+Luk-ESC@users.noreply.github.com>
2022-10-16 22:49:20 +0200

    change if elif chain to dictionary
    also fixed spelling

--- cogs/lib/values.py
+++ cogs/lib/values.py
@@ -1,15 +1,14 @@
-#Very Baisic Api
+#Very Basic Api
 import os
-
 import youtube_dl
 
 
 class values():
     def getData(value):
-        guild1_id=1009880989318791289
-        guild2_id=850066291225133068
-        guild3_id=977477205573652518
-        guilds=[guild1_id, guild2_id,guild3_id]
+        guild1_id = 1009880989318791289
+        guild2_id = 850066291225133068
+        guild3_id = 977477205573652518
+        guilds = [guild1_id, guild2_id,guild3_id]
 
         welcomeEnabledServerID = 977477205573652518
         welcome_channelID = 977477205573652521
@@ -21,22 +20,17 @@ class values():
 
         token = os.getenv("BOTTOKEN")
         youtube_api_key = os.getenv("YTTOKEN")
-
-        if value.lower() == "guilds":
-            return guilds
-        elif value.lower() == "color":
-            return embedColor
-        elif value.lower() == "prefix":
-            return prefix
-        elif value.lower() == "wlc_chnl":
-            return welcome_channelID
-        elif value.lower() == "guild_name":
-            return server_name
-        elif value.lower() == "wlc_message":
-            return welcome_message
-        elif value.lower() == "token":
-            return token
-        elif value.lower() == "yt_api_key":
-            return youtube_api_key
-        elif value.lower() == "wlc_enabled_guild":
-            return welcomeEnabledServerID
+        
+        data = {
+            "guilds": guilds,
+            "color": embedColor,
+            "prefix": prefix,
+            "wlc_chnl": welcome_channelID,
+            "guild_name": server_name,
+            "wlc_message": welcome_message,
+            "token": token,
+            "yt_api_key": youtube_api_key,
+            "wlc_enabled_guild": welcomeEnabledServerID
+        }
+        
+        return data.get(value.lower())
