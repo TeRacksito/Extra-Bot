752c15cfb6164663a516bd216d98724025509c59
Ahmed <ahmedthecat3457@gmail.com>
2022-11-09 18:22:46 +0300

    Added a program that configs the bot token

--- cogs/lib/values.py
+++ cogs/lib/values.py
@@ -8,14 +8,15 @@ class values():
         guild1_id = 1009880989318791289
         guild2_id = 850066291225133068
         guild3_id = 977477205573652518
-        guilds = [guild1_id, guild2_id,guild3_id]
+        guild4_id = 937639594030153758
+        guilds = [guild1_id, guild2_id,guild3_id,guild4_id]
 
         welcomeEnabledServerID = 977477205573652518
         welcome_channelID = 977477205573652521
         server_name = f"Extra Bot Discord Server"
         welcome_message = f"Welcome This is {server_name} which was made for stuff about the extra bot"
 
-        embedColor=0x2852fa
+        embedColor=0x83858a
         prefix="-"
 
         token = os.getenv("BOTTOKEN")
