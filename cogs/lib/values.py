2d9eec67e614ddcd4a57c0d4bf585c819b5190b8
Ahmed <ahmedthecat3457@gmail.com>
2022-11-10 17:01:00 +0300

    Added A Configurator

--- cogs/lib/values.py
+++ cogs/lib/values.py
@@ -1,6 +1,7 @@
 #Very Basic Api
 import os
 import youtube_dl
+from configparser import ConfigParser
 
 
 class values():
@@ -8,19 +9,20 @@ class values():
         guild1_id = 1009880989318791289
         guild2_id = 850066291225133068
         guild3_id = 977477205573652518
-        guild4_id = 937639594030153758
-        guilds = [guild1_id, guild2_id,guild3_id,guild4_id]
+        guilds = [guild1_id, guild2_id,guild3_id]
 
         welcomeEnabledServerID = 977477205573652518
         welcome_channelID = 977477205573652521
         server_name = f"Extra Bot Discord Server"
         welcome_message = f"Welcome This is {server_name} which was made for stuff about the extra bot"
 
-        embedColor=0x83858a
+        embedColor=0x2852fa
         prefix="-"
 
-        token = os.getenv("BOTTOKEN")
-        youtube_api_key = os.getenv("YTTOKEN")
+        config = ConfigParser()
+        config.read("..\..\options.ini")
+        token = config["options"]["bot_token"]
+        youtube_api_key = config["options"]["yt_api"]
         
         data = {
             "guilds": guilds,
