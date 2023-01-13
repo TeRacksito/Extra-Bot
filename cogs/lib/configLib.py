065a1e90f73d5ce45d8896e1b3780a4ddbbc4567
Ahmed <93637332+Ahmed3457@users.noreply.github.com>
2023-01-13 16:00:33 +0000

    Changes to the configLib

--- cogs/lib/configLib.py
+++ cogs/lib/configLib.py
@@ -1,15 +1,16 @@
 import json
 
 class Config():
-    def __init__(self, guild_id, guild_name):
+    def __init__(self, guild_id, guild_name, welcome_chan_id):
         self.guild_id = guild_id
         self.guild_name = guild_name
 
         data = {
             f"server_{self.guild_id}":{
                 "guild_id": guild_id,
-                "guild_name": str(guild_name)
-                "welcome_enabled": False
+                "guild_name": str(guild_name),
+                "welcome_enabled": False,
+                "welcome_chan_id": 0
             }
         }
 
@@ -36,8 +37,21 @@ class Config():
         data = {
             f"server_{guild_id}":{
                 "guild_id": guild_id,
-                "guild_name": str(guild_name)
-                "welcome_enabled": status
+                "guild_name": str(guild_name),
+                "welcome_enabled": status,
+                "welcome_chan_id": welcome_chan_id
+            }
+        }
+        with open("config/servers.json", "r") as conf:
+            json.dump(data, conf)
+
+    def set_welcome_status(self, guild_id, channel):
+        data = {
+            f"server_{guild_id}":{
+                "guild_id": guild_id,
+                "guild_name": str(guild_name),
+                "welcome_enabled": status,
+                "welcome_chan_id": welcome_chan_id
             }
         }
         with open("config/servers.json", "r") as conf:
