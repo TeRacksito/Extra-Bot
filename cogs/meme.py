94c99b4ef0630e6306442ce88d60386a6a3a23f8
Ahmed <93637332+Ahmed3457@users.noreply.github.com>
2023-01-27 16:23:29 +0000

    Added a /meme command

---
+++ cogs/meme.py
@@ -0,0 +1,24 @@
+import nextcord, sys
+from nextcord.ext import commands
+sys.path.insert(1, 'cogs\lib')
+import values as v
+from memeApi import gen_meme
+
+guilds=v.values.getData("guilds")
+embedColor=v.values.getData("color")
+
+class meme(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @nextcord.slash_command(guild_ids=guilds, description="Generates an SFW meme and sends it to the chat, this command sometimes works and sometimes not", force_global = True, dm_permission = True)
+    async def meme(self, interaction: nextcord.Interaction):
+            await interaction.response.defer()
+            meme = gen_meme()
+            meme_embed = nextcord.Embed(title="Here's a meme", color=embedColor)
+            meme_embed.set_image(url = meme)
+            await interaction.followup.send(embed=meme_embed)
+        
+# Setup
+def setup(client):
+    client.add_cog(meme(client))
