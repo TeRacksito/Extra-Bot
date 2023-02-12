83fddcba9708212e9a6a37cca30bada753814dfb
Ahmed <93637332+Ahmed3457@users.noreply.github.com>
2023-02-12 20:14:18 +0000

    Initial setup for bot dashboar

--- cogs/lib/values.py
+++ cogs/lib/values.py
@@ -19,6 +19,12 @@ class values():
         token = config["options"]["bot_token"]
         youtube_api_key = config["options"]["yt_api"]
 
+        with open("config.toml", mode="rb") as fp:
+            config = tomli.load(fp)
+        bot_id=config["options_dashboard"]["bot_id"]
+        client_id=config["options_dashboard"]["client_id"]
+        secret=config["options_dashboard"]["client_secret"]
+
         if not token:
             tkn = config["options"]["bot_token_env"]
             token = os.getenv("tkn")
@@ -31,7 +37,10 @@ class values():
             "color": embedColor,
             "prefix": prefix,
             "token": token,
-            "yt_api_key": youtube_api_key
+            "yt_api_key": youtube_api_key,
+            "bot_id": bot_id,
+            "client_id": client_id,
+            "secret": secret
         }
         
         return data.get(value.lower())
