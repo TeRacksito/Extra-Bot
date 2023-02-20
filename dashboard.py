626312926471502a74254c640fb4c7544e264f2b
Ahmed <93637332+Ahmed3457@users.noreply.github.com>
2023-02-20 17:20:26 +0000

    Fixed this madness

--- dashboard.py
+++ dashboard.py
@@ -11,41 +11,42 @@ app.secret_key = "my"
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
 
-try:
-    app.config["DISCORD_CLIENT_ID"] = int(v.getData("client_id"))
-    app.config["DISCORD_CLIENT_SECRET"] = intv.getData("secret")
-    app.config["DISCORD_CLIENT_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"
-    app.config["DISCORD_BOT_TOKEN"] = v.getData("token")
-    dis = DiscordOAuth2Session(app)
-    
-    @app.route("/test/")
-    async def test():
-        return "test worked !"
-    @app.route("/login/")
-    async def login():
-        return await dis.create_session()
-    
-    @app.route("/callback/")
-    async def callback():
-        try:
-            await dis.callback()
-        except:
-            return redirect(url_for("login"))
-        return redirect(url_for("dashboard"))
-    
-    @app.route("/")
-    async def mainPage():
-        return await render_template("index/index.html")
+secret=values.getData("secret")
+app.config["DISCORD_CLIENT_SECRET"] = secret
+app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"
+app.config["DISCORD_CLIENT_ID"] = int(values.getData("client_id"))
+app.config["DISCORD_BOT_TOKEN"] = values.getData("token")
+dis = DiscordOAuth2Session(app)
 
-    @app.route("/dash/")
-    async def dash():
-        user = await dis.fetch_user()
-        return render_template("dash/dashboard.html", user=user)
-except:
-    def do_nothing():
-        #This does nothing bc i have no idea what i should do to "do nothing"
-        return
+@app.route("/login")
+async def login():
+    return await dis.create_session()
+
+
+@app.route("/")
+async def mainPage():
+    return await render_template("index/index.html")
+
+@app.route("/dash")
+async def dash():
+    user = await dis.fetch_user()
+    return render_template("dash/dashboard.html", user=user)
+
+@app.route("/callback")
+async def callback():
+    try:
+        await dis.callback()
+    except:
+        return redirect(url_for("login"))
+    return redirect(url_for("dashboard"))
 
 if __name__ == "__main__":
     port=2010
-    app.run()
+    app.run(port=port, debug=True)
+# from quart import Quart
+# app = Quart(__name__)
+
+# @app.route("/")
+# def index():
+#     return "hey"
+# app.run(port=2010)
